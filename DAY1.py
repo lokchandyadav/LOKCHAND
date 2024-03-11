18aff8e5bf1f65953d7bbb82a5cbfd0b5539790b
lokchandyadav <162981151+lokchandyadav@users.noreply.github.com>
2024-03-11 16:12:35 +0530

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,107 @@
+
+        DAY-1 Theory part
+        
+   1. BARAGAINED CHARACTERS
+
+    #Controls and data structures-->ABC language
+
+    #Strings and its format -->from c
+
+    #Regular expressions --> Pearl
+
+    #Exception handling --> Module 3
+
+    #Magic mrthods from --> fortron
+    
+  
+   2. What kind of language is pthon
+
+    #python is a
+
+    #> object oriented programming language
+    #> Dynamically typed language
+    #> Scripting language
+    #> Interpreted language
+    #> Compiled language
+    #> Functional programming language
+
+   3. Flavours of python
+    
+    #> Cpython-Written in c, Most common implementation of python
+    
+    #> Jython- java implementation in python
+    
+    #> Ironpython- Implementation of the python programming language which is tightly integrated with NET
+    
+    #> Brython- Browser Python, runs in browsers
+    
+    #> RubyPython- Bridge between python and Ruby Interpreters
+    
+    #> Micropython- Runs in Micro controllers
+
+
+    4.Print has 5 parameters
+    
+    #Object
+    #sep
+    #end
+    #file
+    #flush
+    
+     #>object
+     #-->the value to be printed is object
+     
+     #>sep-->seperator
+     
+     #Example
+     #a=9
+     #b=8
+     #c=89
+     #print(a,b,c, sep='$')
+     
+     #>end-->which is used to add the string at the end of the value,
+      #It will not keep the command to saty in same line
+
+      #Example
+      #print(str(a), b, end='%%')
+      #print(str(c))
+     
+     
+
+      !Variables--->
+
+  #?variable is a name which is used to identify the value of variable
+  #?which is stored in a specific memory location    
+   
+    !Rules to define variables
+
+  # 1.) Variables have to start with only alphabets,underscores
+  # 2.) The character following the 1st character can be alphabets,numbers,underscores
+  # 3.) No spaces are allowed
+  # 4.) No special characters like !@#$%^&*()+
+  # 5.) Case sensitive
+  # 6.) Keywords are not allowed as variables
+
+    # ! ways to define variables
+
+    #a = 8
+    #b = 9
+    #print(a, b)
+                                      
+   #a=7, b=9 #error
+    #print(a)
+    #print(b)
+
+    #a=7
+    #b=7
+
+    # a = b =7
+    #print(a, b)
+
+    #a=8, 7,9.89, "hello"
+    #print(a)
+    #print(type(a))
+
+    #a,b = 78 #error
+    #print(a, b)
+
